78cb55b27cbd5652941e7b8499fcbbae314137f5
Kristen <gandalf1713@gmail.com>
2020-11-29 08:23:58 -0800

    adding changes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/signin', {
+    fetch('https://stormy-ocean-79221.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
